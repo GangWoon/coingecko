3c6cf0d30c1b97aa49dfa5568b9961ad1a5a0967
GangWoon <gangwoon2018@gmail.com>
2024-05-27 03:42:02 +0900

    Add view controller task life cycle method

---
+++ Sources/SearchFeature/BaseViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+open class BaseViewController: UIViewController {
+  public var _task: Task<Void, Never>?
+  
+  open override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    _task = Task { await task() }
+  }
+  
+  open override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    _task?.cancel()
+  }
+  
+  open func task() async { }
+}
