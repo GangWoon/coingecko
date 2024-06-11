2783961dfdedd44735eba07d6761ba61125420b8
GangWoon <gangwoon2018@gmail.com>
2024-06-12 01:59:58 +0900

    Update package version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
@@ -74,5 +74,6 @@ let package = Package(
     .testTarget(
       name: "SearchFeatureTests",
       dependencies: ["SearchFeature"]),
-  ]
+  ],
+  swiftLanguageVersions: [.v6]
 )
